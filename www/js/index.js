6a4c155eb449ab8a7d28c45a29cdbba082191767
Mahesh K S <924141@01HW384952.local>
2016-12-20 11:52:26 +0530

    Commiting file to master

---
+++ www/js/index.js
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        //callNativeFunction();
+        document.getElementById('capture-image').addEventListener('click',function(){
+              var element = document.getElementsByClassName('red-container');
+              html2canvas(element, {
+                onrendered: function(canvas) {
+                    var dataURL = canvas.toDataURL();
+                    document.getElementById("screenshot").src = dataURL;
+                }
+              });
+
+        },false);
+
+        document.getElementById('email-image').addEventListener('click',function(){
+              var imgData = document.getElementById("screenshot").src;
+              cordova.plugins.email.isAvailable(
+                function (isAvailable) {
+                    cordova.plugins.email.open({
+                        to:      '',
+                        cc:      '',
+                        bcc:     [],
+                        subject: '',
+                        //attachments: imgData,
+                        body:    '<img src="'+imgData+'">' 
+                    });
+               });
+
+        },false);
+
+        
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        
+    }
+};
+
+app.initialize();
